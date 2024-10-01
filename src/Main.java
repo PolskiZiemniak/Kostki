895055641edb8c87e1727eb807958703b1abada7
PolskiZiemniak <polskiziemniak2018@gmail.com>
2024-10-01 09:30:56 +0200

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String wybor;
         int iloscKostek;
         iloscKostek = wprowadzIloscKostek();
-
-        int[] tab = losujliczby(iloscKostek);
-        for(int i = 0; i < iloscKostek; i++){
-            System.out.println("Kostka "+i+" "+tab[i]);
+        while(true) {
+            int[] tab = losujliczby(iloscKostek);
+            for (int i = 0; i < iloscKostek; i++) {
+                int a = i+1;
+                System.out.println("Kostka " + a + ": " + tab[i]);
+            }
+            System.out.println("Liczba uzyskanych punktów: "+obliczPunkty(tab));
+            System.out.println("Czy chcesz powtórzyć? (t/n)");
+            wybor = scan.nextLine();
+            if(wybor.equals("n")){
+                break;
+            }
         }
-        System.out.println(obliczPunkty(tab));
 
     }
+
+    /*************************************************************
+     * nazwa:                  wprowadzIloscKostek
+     * opis:                   prosi użytkownika o ilość kostek z zakresu 3-10
+     * parametry:              brak
+     * zwracany typ i opis:    int - zwraca ilość wpisanych kostek
+     * autor:                  85926928513
+    **************************************************************/
     public static int wprowadzIloscKostek(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Ile kostek chcesz rzucić (3 - 10)");
@@ -31,6 +48,14 @@ public class Main {
         }
         return ilosc;
     }
+
+    /*************************************************************
+     * nazwa:                  losujLiczby
+     * opis:                   losuje wybraną ilość liczb z zakresu 1-6
+     * parametry:              int i - ilość losowanych kostek
+     * zwracany typ i opis:    int[] - zawiera każdą wylosowaną liczbę
+     * autor:                  85926928513
+     ************************************************************/
     public static int[] losujliczby(int i){
         Random rand = new Random();
         int[] tab = new int[i];
@@ -39,6 +64,14 @@ public class Main {
         }
         return tab;
     }
+
+    /*************************************************************
+     * nazwa:                  obliczPunkty
+     * opis:                   sumuje razem wszystkie powtarzające się liczby
+     * parametry:              int[] tab - przyjmuje tablice z wylosowanymi liczbami
+     * zwracany typ i opis:    int - zawiera sumę powtarzających się liczb
+     * autor:                  85926928513
+     ************************************************************/
     public static int obliczPunkty(int[] tab){
         int sum = 0;
         boolean[] counted = new boolean[tab.length];
